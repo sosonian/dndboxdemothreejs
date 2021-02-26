f825ac6d8356fc001a2a536644e615c017a49788
sosonian <sosonian@gmail.com>
2021-02-26 10:58:03 +0000

    commit code

--- src/DemoApp.js
+++ src/DemoApp.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
 import orbControls from './OrbitControls';
-import {DnDContainer, DnDLayout} from 'dnd-box'
+import Stats from 'stats.js';
+import {DnDContainer, DnDBackgroundComponent, DnDLayout} from 'dnd-box'
 import { CSS3DRenderer, CSS3DObject } from './Utilities/CSS3DRenderer';
 
 
@@ -15,26 +16,41 @@ class DemoApp extends Component{
     }
 
     componentDidMount(){
+        this.deployState();
         this.frustumSize = 500
-        const aspect = window.innerWidth / window.innerHeight;
-		this.camera1 = new THREE.OrthographicCamera( this.frustumSize * aspect / - 2, this.frustumSize * aspect / 2, this.frustumSize / 2, this.frustumSize / - 2, 1, 1000 );	
+        const aspect = 1920 / 800;
+		this.camera1 = new THREE.PerspectiveCamera( 
+            100,
+            aspect,
+            0.1,
+            1000 
+        );	
 		this.scene = new THREE.Scene();
-        //this.scene2 = new THREE.Scene();
-        this.camera1.position.set( - 200, 200, 200 );
+        this.camera1.position.set( - 20, 20, 20 );
 		this.scene.background = new THREE.Color( 0xf0f0f0 );
-        this.createPlane(100, 100,'chocolate',new THREE.Vector3( - 50, 0, 0 ),new THREE.Euler( 0, - 90 * THREE.MathUtils.DEG2RAD, 0 ))
-
+        this.createPlane(20, 20,'chocolate',new THREE.Vector3( 0, 10, 0 ),new THREE.Euler( 0, - 90 * THREE.MathUtils.DEG2RAD, 0 ))
         this.renderer = new THREE.WebGLRenderer();
 		this.renderer.setPixelRatio( window.devicePixelRatio );
-		this.renderer.setSize( window.innerWidth, window.innerHeight );
+		this.renderer.setSize( 1920, 800);
 		this.mount.appendChild( this.renderer.domElement );
+        this.sceneGrid = new THREE.GridHelper(50,50)
+        this.scene.add(this.sceneGrid)
 
-        //this.renderer2 = new CSS3DRenderer();
-		//this.renderer2.setSize( window.innerWidth, window.innerHeight );
-		//this.renderer2.domElement.style.position = 'absolute';
-		//this.renderer2.domElement.style.top = 0;
-		//document.body.appendChild( this.renderer2.domElement );
+        this.camera2 = new THREE.PerspectiveCamera(
+            70,
+            200 / 200,
+            0.1,
+            1000
+        )
+        this.camera2.position.z =20
+        this.camera2.position.x =0
+        this.camera2.position.y =1
+        this.camera2.lookAt(0,0,0)
+        this.renderer2 = new THREE.WebGLRenderer()
+        this.renderer2.setClearColor(0xeeeeee, 1.0) 
 
+
+        this.orbitControlSet()
         this.animate()
         
     }
@@ -47,38 +63,45 @@ class DemoApp extends Component{
     componentWillUnmount() {
     }
 
+    deployState = () => {
+        this.stats = new Stats()
+        this.stats.setMode(0) // FPS mode
+        this.stats.domElement.style.position = 'absolute'
+        this.mainBody.appendChild(this.stats.domElement)
+    }
+
     animate=()=>{
+        //console.log("DemoApp animate")
+        //console.log(this.mesh.rotation.x)
+        this.stats.begin();
         //let c1Background = new THREE.Color('rgb(255,255,255)')
         //this.renderer.setClearColor(c1Background)
         this.renderer.render(this.scene, this.camera1)
-        this.mesh.rotateX += 0.1
+        this.mesh.rotation.x += 0.01
         //this.renderer2.render(this.scene2, this.camera );
+        this.stats.end();
+        window.requestAnimationFrame(this.animate)
     }
 
-    
+    orbitControlSet = () => {
+        this.cameraControl = new orbControls(this.camera1)
+    }
 
     createPlane=(width, height, cssColor, pos, rot)=>{   
-        // const element = document.createElement( 'div' );
-		// element.style.width = width + 'px';
-		// element.style.height = height + 'px';
-		// element.style.opacity = 0.75;
-		// element.style.background = cssColor;
-
-		// const object = new CSS3DObject( element );
-		// object.position.copy( pos );
-		// object.rotation.copy( rot );
-		// this.scene2.add( object );
-
-        const material = new THREE.MeshBasicMaterial( { color: 0x000000, wireframe: true, wireframeLinewidth: 1, side: THREE.DoubleSide } );
+        const material = new THREE.MeshBasicMaterial( { color: 0x0000ff, side: THREE.DoubleSide } );
 
 		const geometry = new THREE.PlaneGeometry( width, height );
 		this.mesh = new THREE.Mesh( geometry, material );
-		//mesh.position.copy( object.position );
-		//mesh.rotation.copy( object.rotation );
+		this.mesh.position.copy( pos );
 		this.scene.add( this.mesh );
     }
 
+    onClick=()=>{
+        console.log("THREE Scene be clicked !")
+    }
+
     render(){
+        console.log("DemoApp render")
         let boxesSetting=[
             {
                 boxID:'A',
@@ -117,21 +140,20 @@ class DemoApp extends Component{
             }
         ]
 
-        const myStyle = {
-            width: '100%',
-            height: 800,
-        }
-
         return(
-            <div onMouseDown={this.threeDLayerMouseDown} onMouseMove={this.ThreeDLayerMouseMove} onMouseUp={this.threeDLayerMouseUp} onContextMenu={this.onContextMenu} style={myStyle} ref={(mount) => { this.mount = mount }}>
+            <div ref={(mainBody)=>{this.mainBody = mainBody}} style={{border:"1px solid black"}}>
                 <DnDLayout backgroundColor={'pink'} width={1920} height={800} boxColor={''} boxHeaderColor={''} boxTabColor={''} boxHeaderHoverColor={''} boxTabHoverColor={''} boxTabSelectedColor={''} iconHoverColor={''} boxTabRadius={'0px 10px 0px 0px'} boxesSetting={boxesSetting} openContainer={this.state.showContainer}  tabHeight={25}>
+                    <DnDBackgroundComponent>
+                        <div onClick={this.onClick} ref={(mount) => { this.mount = mount }}>
+                        </div>
+                    </DnDBackgroundComponent>
                     <DnDContainer containerTabTitle={"front view"} containerID={1} boxID={'A'}>
-                        <div>
+                        <div ref={(frontView) => { this.frontView = frontView }}>
                             {"front view"}
                         </div>
                     </DnDContainer>
                     <DnDContainer containerTabTitle={"side view"} containerID={2} boxID={'B'}>
-                        <div>
+                        <div ref={(frontView) => { this.frontView = frontView }}>
                             {"side view"}
                         </div>
                     </DnDContainer>
@@ -151,7 +173,7 @@ class DemoApp extends Component{
                         </div>
                     </DnDContainer>
                 </DnDLayout>
-            </div>  
+            </div>
         )
     }
 }
